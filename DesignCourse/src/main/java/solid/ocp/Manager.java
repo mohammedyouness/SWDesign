dc4adcc87cde181c540d3641c48318b449ba9439
mohammedyouness <dev.mohamedyunus@gmail.com>
2023-10-14 11:51:28 +0300

    removed unnecessary files

--- DesignCourse/src/main/java/solid/ocp/Manager.java
+++
@@ -1,8 +0,0 @@
-package solid.ocp;
-
-public class Manager extends Employee{
-    @Override
-    public float calcHoursBonus(float hours) {
-        return (((basicSalary/30) / 8) * hours) * 2;
-    }
-}
--- DesignCourse/src/main/java/solid/ocp/Regular.java
+++
@@ -1,8 +0,0 @@
-package solid.ocp;
-
-public class Regular extends Employee{
-    @Override
-    public float calcHoursBonus(float hours) {
-        return ((basicSalary/30) / 8) * hours;
-    }
-}
